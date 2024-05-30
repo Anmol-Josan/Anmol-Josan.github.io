200205ee0395a591dc94e35e0e057724e776ddab
ajosan123 <ajosan@eastsideprep.org>
2024-05-30 09:45:42 -0700

    made about page

--- final-portfolio/JS/script.js
+++ final-portfolio/JS/script.js
@@ -4,3 +4,13 @@ window.addEventListener('DOMContentLoaded', () => {
             (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
     });
 });
+document.addEventListener('keydown', (event) => {
+
+    if (event.key === 'Escape') {
+        const isNotCombinedKey = !(event.ctrlKey || event.altKey || event.shiftKey);
+        if (isNotCombinedKey) {
+            document.querySelector('.nav-links').style.display =
+            (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+        }
+    }
+});
